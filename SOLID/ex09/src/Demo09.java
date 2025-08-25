8729f88a4faa0fc1e0b5b1edf6867093a31770c2
Prem Shah <prem.nbshah@gmail.com>
2025-08-25 11:22:04 +0530

    Refactored exercise 9

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,11 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        OrderRepository sqlRepo = new SqlOrderRepository();
+        OrderController sqlController = new OrderController(sqlRepo);
+        sqlController.create("ORD-1");
+
+        OrderRepository noSqlRepo = new NoSqlOrderRepository();
+        OrderController noSqlController = new OrderController(noSqlRepo);
+        noSqlController.create("ORD-2");
     }
 }
