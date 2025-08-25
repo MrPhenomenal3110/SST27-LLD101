4bc960169a014db10e758911fb7fbc2e0d9ffa35
Prem Shah <prem.nbshah@gmail.com>
2025-08-25 11:27:40 +0530

    Refactored exercise 10

--- SOLID/ex10/src/Demo10.java
+++ SOLID/ex10/src/Demo10.java
@@ -1,5 +1,11 @@
 public class Demo10 {
     public static void main(String[] args) {
-        new ReportService().generate();
+        Logger fileLogger = new FileLogger();
+        ReportService fileReportService = new ReportService(fileLogger);
+        fileReportService.generate();
+
+        Logger consoleLogger = new ConsoleLogger();
+        ReportService consoleReportService = new ReportService(consoleLogger);
+        consoleReportService.generate();
     }
 }
