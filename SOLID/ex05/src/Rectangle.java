01c0a3186239ec219fa992ef8b192a9cd23b2fa5
Prem Shah <prem.nbshah@gmail.com>
2025-08-25 10:45:07 +0530

    Refactored exercise 5

--- SOLID/ex05/src/Rectangle.java
+++ SOLID/ex05/src/Rectangle.java
@@ -1,6 +1,14 @@
-public class Rectangle {
+public class Rectangle extends Shape {
     protected int w,h;
-    void setWidth(int w){ this.w = w; }
-    void setHeight(int h){ this.h = h; }
-    int area(){ return w*h; }
+    public void setWidth(int w){
+        this.w = w; 
+    }
+    public void setHeight(int h){
+        this.h = h;
+    }
+
+    @Override 
+    public int area(){
+        return w*h;
+    }
 }
